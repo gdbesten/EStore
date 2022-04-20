c4e4a901c174a5f71209afbc2a44debdca1c0d08
Gijs den Besten <gijsbert.den.besten@gmail.com>
2022-04-20 22:16:25 +0200

    ShippingCalculatorTest and CheckOutEngineTest happy tests added

---
+++ ShoppingCartServiceTests/CheckOutEngineTest.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using ShoppingCartService.BusinessLogic;
+using ShoppingCartService.DataAccess.Entities;
+using ShoppingCartService.Mapping;
+using ShoppingCartService.Models;
+using Xunit;
+
+namespace ShoppingCartServiceTests
+{
+    public class CheckOutEngineTest
+    {
+        private readonly Address _address;
+        private readonly ShippingCalculator _shippingCalculator
+            ;
+
+        private readonly CheckOutEngine _checkOutEngine;
+        private readonly IMapper _mapper;
+
+        public CheckOutEngineTest()
+        {
+            _address = new Address
+            {
+                Street = "Street",
+                City = "City",
+                Country = "Country"
+            };
+
+
+            _shippingCalculator = new ShippingCalculator(_address);
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfile()));
+
+            _mapper = config.CreateMapper(); _checkOutEngine = new CheckOutEngine(_shippingCalculator, _mapper);
+        }
+
+        [Fact]
+        public void StandardHappyTest()
+        {
+            // Arrange
+            var cart = new Cart
+            {
+                CustomerType = CustomerType.Standard,
+                ShippingAddress = _address,
+                Items = new List<Item>
+                {
+                    new Item
+                    {
+                        ProductId = "0001",
+                        ProductName = "Product 1",
+                        Price = 10,
+                        Quantity = 5
+                    }
+                }
+            };
+
+            // Act
+            var result = _checkOutEngine.CalculateTotals(cart);
+
+            // Assert, 5 * 10 + 5 = 55
+            Assert.Equal(55, result.Total);
+
+        }
+
+        [Fact]
+        public void PremiumHappyTest()
+        {
+            // Arrange
+            var cart = new Cart
+            {
+                CustomerType = CustomerType.Premium,
+                ShippingAddress = _address,
+                Items = new List<Item>
+                {
+                    new Item
+                    {
+                        ProductId = "0001",
+                        ProductName = "Product 1",
+                        Price = 99,
+                        Quantity = 5
+                    }
+                }
+            };
+
+            // Act
+            var result = _checkOutEngine.CalculateTotals(cart);
+
+            // Assert, 5 * 99 + 5 = 500 * 0.9 = 450
+            Assert.Equal(450, result.Total);
+
+        }
+    }
+}
---
+++ ShoppingCartServiceTests/ShippingCalculatorTest.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using ShoppingCartService.BusinessLogic;
+using ShoppingCartService.DataAccess.Entities;
+using ShoppingCartService.Models;
+using Xunit;
+
+namespace ShoppingCartServiceTests
+{
+    public class ShippingCalculatorTest
+    {
+        private readonly Address _address;
+        private readonly ShippingCalculator _shippingCalculator;
+
+        public ShippingCalculatorTest()
+        {
+            _address = new Address
+            {
+                Street = "Street",
+                City = "City",
+                Country = "Country"
+            };
+            _shippingCalculator = new ShippingCalculator(_address);
+        }
+
+        [Fact]
+        public void SingleProductHappyTest()
+        {
+            // Arrange
+            var cart = new Cart
+            {
+                CustomerType = CustomerType.Standard,
+                ShippingAddress = _address,
+                Items = new List<Item>
+                {
+                    new Item
+                    {
+                        ProductId = "0001",
+                        ProductName = "Product 1",
+                        Price = 10,
+                        Quantity = 5
+                    }
+                }
+            };
+
+            // Act
+            var result = _shippingCalculator.CalculateShippingCost(cart);
+
+            // Assert, 5 * 1 = 5
+            Assert.Equal(5, result);
+
+        }
+        [Fact]
+        public void DoubleProductHappyTest()
+        {
+            // Arrange
+            var cart = new Cart
+            {
+                CustomerType = CustomerType.Standard,
+                ShippingAddress = _address,
+                Items = new List<Item>
+                {
+                    new Item
+                    {
+                        ProductId = "0001",
+                        ProductName = "Product 1",
+                        Price = 10,
+                        Quantity = 5
+                    },
+                    new Item
+                    {
+                        ProductId = "0002",
+                        ProductName = "Product 2",
+                        Price = 10,
+                        Quantity = 15
+                    }
+                }
+            };
+
+            // Act
+            var result = _shippingCalculator.CalculateShippingCost(cart);
+
+            // Assert, 5 * 1 + 15 * 1 = 20
+            Assert.Equal(20, result);
+
+        }
+    }
+}
